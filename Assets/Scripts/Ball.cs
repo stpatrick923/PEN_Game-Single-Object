5a5074a3463d919b1de9c4b6f01f357965668d33
stpatrick923 <stpatrick923@gmail.com>
2015-08-23 21:37:41 -0400

    Initial Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private bool beingDragged = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.z = 0;
+		transform.position = pos;
+	}
+
+	bool isBeingDragged()
+	{
+		return beingDragged;
+	}
+
+	void setBeingDragged(bool currentlyBeingDragged)
+	{
+		beingDragged = currentlyBeingDragged;
+	}
+}
---
+++ Assets/Scripts/CreatorCube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatorCube : MonoBehaviour {
+
+	private bool beingDragged = false;
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		beingDragged = true;
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+
+	void OnMouseUp()
+	{
+		beingDragged = false;
+	}
+}
